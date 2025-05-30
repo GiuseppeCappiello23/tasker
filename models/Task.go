ae6fd523aab93f62a9edeb97df21cb7174d0dbc1
solyps <giuseppe.cappiello23@outlook.com>
2025-05-30 13:41:32 +0200

    Refactor Task model and database operations for task creation and listing

--- models/Task.go
+++ models/Task.go
@@ -2,28 +2,57 @@ package models
 
 import (
 	"database/sql"
-	"log"
 
 	_ "github.com/mattn/go-sqlite3"
 )
 
 type Task struct {
-	Name        string
+	Title       string
 	DueDate     string
 	Status      string
 	Description string
 }
 
-func (t *Task) NewTask(s string) error {
+func (t *Task) NewTask(task Task) error {
 	db, err := sql.Open("sqlite3", "./miodb.sqlite")
 	if err != nil {
-		log.Fatal(err)
+		return err
 	}
 	defer db.Close()
 
-	stmt, err := db.Prepare("INSERT INTO utenti(nome, email) VALUES(?, ?)")
+	stmt, err := db.Prepare("INSERT INTO Task(Title, Description, DueDate, Status) VALUES(?, ?, ?, ?)")
 	if err != nil {
-		log.Fatal(err)
+		return err
 	}
 	defer stmt.Close()
+
+	_, err = stmt.Exec(task.Title, task.Description, task.DueDate, task.Status)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (t *Task) ListTasks() error {
+	db, err := sql.Open("sqlite3", "./miodb.sqlite")
+	if err != nil {
+		return err
+	}
+	defer db.Close()
+
+	stmt, err := db.Prepare("SELECT * FROM Task")
+	if err != nil {
+		return err
+	}
+	defer stmt.Close()
+
+	results, err := stmt.Exec()
+	if err != nil {
+		return err
+	}
+	
+	for r := range results {
+
+	}
 }
